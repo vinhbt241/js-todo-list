2cadb787ab57fbe032abec362c9012f79b0b3485
Vinh <vinhbuith32412000@gmail.com>
2022-05-12 22:13:31 +0700

    Add form to create new task

--- src/DOMController.js
+++ src/DOMController.js
@@ -1,6 +1,9 @@
 import { format } from "date-fns";
+import { Task } from "./task";
+import { App } from "./app";
 
 const RenderController = () => {
+
   const renderProjectList = (projects) => {
     const projectList = document.createElement('div');
   
@@ -20,9 +23,11 @@ const RenderController = () => {
 
   const renderTaskList = (project) => {
     const taskList = document.createElement('div');
+    taskList.setAttribute("projectid", project.ID);
+    taskList.classList.add("task-list");
 
     project.tasks.forEach(task => {
-      const taskItem = document.createElement('button');
+      const taskItem = document.createElement('div');
       taskItem.id = task.ID;
 
       const taskName = document.createElement('h3');
@@ -60,6 +65,29 @@ const RenderController = () => {
 
     return taskList
   }
+
+  const newTaskForm = document.getElementById("new-task");
+  newTaskForm.addEventListener('submit', e => {
+    e.preventDefault();
+    // Create new task
+    let name = newTaskForm['new-task-name'].value;
+    let info = newTaskForm['new-task-info'].value;
+    let dueDate = Date.parse(newTaskForm['new-task-due-date'].value);
+    const newTask = Task(name, info, dueDate);
+    // Find current project id
+    const taskList = document.querySelector(".task-list");
+    let displayProjectID = taskList.getAttribute('projectid');
+    // Add task to current project
+    App.projects.forEach((project) => {
+      if(project.ID == displayProjectID) {
+        project.addTask(newTask);
+        taskList.innerHTML = ""
+        taskList.appendChild(renderTaskList(project));
+        return;
+      }
+    })
+    return;
+  })
   
   return { renderProjectList, renderTaskList }
 }
